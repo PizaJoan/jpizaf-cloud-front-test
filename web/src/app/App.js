989b26886414707b3aba13c92837350d616ccb77
Juan Piza Ferra <pizajoan97@gmail.com>
2024-02-10 09:43:14 +0100

    - Change the border radius of all components (button, input, ...) - Change the default color from blue to green

--- web/src/app/App.js
+++ web/src/app/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ConfigProvider } from "antd";
 
 import LayoutRegister from '../layouts/register';
 import LayoutLogin from '../layouts/login';
@@ -9,17 +10,30 @@ import BookmarksList from "../layouts/bookmarks_list";
 
 function App() {
   return (
-    <BrowserRouter basename={"/"}>
-      <Routes>
-        <Route path="/bookmarks" element={<BookmarksList />} />
-        <Route path="/buildings" element={<BuildingList />} />
-        <Route path="/buildings/:buildingId" element={<BuildingMetrics />} />
-        <Route path="/register" element={<LayoutRegister />} />
-        <Route path="home" element={<LayoutRegister />} />
-        <Route path="/login" element={<LayoutLogin />} />
-        <Route path="/" element={<LayoutRegister />} />
-      </Routes>
-    </BrowserRouter>
+    <ConfigProvider
+        theme={{
+            token: {
+                borderRadius: 6,
+                borderRadiusLG: 8,
+                borderRadiusOuter: 6,
+                borderRadiusSM: 6,
+                borderRadiusXS: 4,
+                colorPrimary: '#00A86B',
+            }
+        }}
+    >
+      <BrowserRouter basename={"/"}>
+        <Routes>
+          <Route path="/bookmarks" element={<BookmarksList />} />
+          <Route path="/buildings" element={<BuildingList />} />
+          <Route path="/buildings/:buildingId" element={<BuildingMetrics />} />
+          <Route path="/register" element={<LayoutRegister />} />
+          <Route path="home" element={<LayoutRegister />} />
+          <Route path="/login" element={<LayoutLogin />} />
+          <Route path="/" element={<LayoutRegister />} />
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   );
 }
 
